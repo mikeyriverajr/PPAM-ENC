9d3c585d018382ee2a3c4163786300585a909da9
mikeyriverajr <mikeyriverajr@gmail.com>
2026-01-06 15:49:16 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'ppam-schedule-v6';
+const CACHE_NAME = 'ppam-schedule-v11';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
-  './app.v6.js',
+  './app.v11.js',
   './icon.png',
   './manifest.json'
 ];

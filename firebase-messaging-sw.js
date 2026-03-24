fc544179523d62895ee3d6b392c4d5e08bff65ae
mikeyriverajr <mikeyriverajr@gmail.com>
2026-03-24 14:57:05 -0300

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5HPI4WY19Om_HmQgJJl6IvXr0XrMmflQ",
+  authDomain: "ppam-beta.firebaseapp.com",
+  projectId: "ppam-beta",
+  storageBucket: "ppam-beta.firebasestorage.app",
+  messagingSenderId: "879252975424",
+  appId: "1:879252975424:web:6e62c58c4b4ba8689d94a5",
+  measurementId: "G-BXVKGLHV9L"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+// This runs in the background to catch the notification
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon.png' // You can add a 192x192 icon.png to your folder later!
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
